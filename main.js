5420fd436e19f523abd0a207ae8644d1271cc2ab
“NematulloNurmamatov” <nematullohataboev@gmail.com>
2023-04-19 20:34:24 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,234 @@
+//begin 
+// kvadratning tomoni a berilgan. Uning perimetri aniqlansin. P = 4 * a
+
+// let a = 5;
+// const P = 4 * a;
+
+// console.log("Perimetri:",P);
+
+
+
+
+
+// begin 2
+//  kvadratning tomoni a berilgan. Uning yuzasi aniqlansin. S = a ** 2
+
+// let a = 4;
+// const S = a ** 2;
+
+// console.log("Yuzasi:",S);
+
+
+
+
+
+// begin 3
+// to'g'ri to'rtburchakning tomonlari a va b berilgan. Unig yuzasi S=a*b; va P = 2 * (a + b) perimetri aniqlansin
+
+// let a = 4;
+// let b = 5;
+// const S = a * b;
+// const P = 2 * (a + b);
+
+// console.log("Yuzasi:", S);
+// console.log("Perimetri:", P);
+
+
+
+
+
+// begin 4
+// Aylananing diametri d berilgan. Uning uzunligi aniqlansin. L = PI * d PI = 3.14
+
+// let d = 13;
+// let PI = 3.14;
+// const L = PI * d;
+
+// console.log("uzunligi:", L);
+
+
+
+
+
+// begin 5
+// Kubnig yon tomoni a berilgan. Uning hajmi V=a**3 va to'la sirti S=6*a**2 aniqlansin
+
+// let a = 2;
+// const V = a ** 3;
+// const S = 6 * (a ** 2);
+
+// console.log("Hajmi:", V);
+// console.log("Sirti:", S);
+
+
+
+
+// begin 6
+// Paralelepepidning tomonlari a, b, c berilgan. Uning hajmi V=a*b*c va to'la sirti S=2*(a*b+b*c+a*c) aniqlansin
+
+// let a = 3;
+// let b = 4;
+// let c = 5;
+
+// const V = a * b * c;
+// const S = 2 * ((a * b)+(b * c)+(a * c));
+
+// console.log("hajmi:", V);
+// console.log("sirti:", S);
+
+
+
+
+
+// Begin 7
+// Doiraning radiusi R berilgan. Uning uzunligi L va yuzasi S aniqlansin. L = 2 * PI * R  S = PI * R ** 2
+
+// let R = 3;
+// const PI = 3.14;
+// const L = 2 * PI * R;
+// const S = PI * R ** 2;
+
+// console.log("Lzunligi:", L);
+// console.log("Yuzasi:", S);
+
+
+
+
+
+// Begin 8
+// Ikkita son a va b berilgan. Ularning o'rta arixmetigi aniqlansin. (a + b) / 2
+
+// let a = 3;
+// let b = 10;
+
+// const m = (a + b) / 2;
+// console.log("urta arifmetigi", m);
+
+
+
+
+
+// Begin 9
+// Ikkita manfiy bo'lmagan a va b berilgan. Ularning o'rta geometrigi aniqlansin. (a * b) ** 1 / 2
+
+// let a = 3;
+// let b = 9;
+// const m = (a + b) **  1/2;
+
+// console.log("urta geometrigi", m);
+
+
+
+
+
+// Begin 10
+// Nolga teng bo'lmagan ikkita son berilgan. Ularning yig'indisi, ko'paytmasi va har birining kvadrati aniqlansin
+
+// let a = 3;
+// let b = 4;
+
+// const yigindi = (a + b);
+// const kupaytma = (a * b);
+// const adaraja = (a ** 2);
+// const bdaraja = (b ** 2);
+
+// console.log("yig'indisi:", yigindi);
+// console.log("ko'paytmasi:", kupaytma);
+// console.log("kvadrati a ning:", adaraja);
+// console.log("kvadrati b ning:", bdaraja);
+
+
+
+
+
+// Begin 11
+// Nolga teng bo'lmagan ikkita son berilgan. Ularning yig'indisi, ko'paytmasi va har birining moduli aniqlansin
+
+// let a = 3;
+// let b = 4;
+// const sum = a + b;
+// const multiples = a * b;
+// const Amodul = (a);
+// const Bmodul = (b);
+
+// console.log("yig'indisi:", sum);
+// console.log("ko'paytmasi:", multiples);
+// console.log("moduli a:", Amodul);
+// console.log("moduli b:", Bmodul);
+
+
+
+
+
+// Begin 12
+// To'g'ri uchburchakning katetlari a va b berilgan. Uning gipotenuzasi c va perimetri P aniqlansin. c = (a ** 2 + b ** 2) ** 1/2 P = a + b + c
+
+// let a = 3;
+// let b = 4;
+// const c = (a ** 2 + b ** 2) ** 1/2;
+// const P = a + b + c;
+
+// console.log("gipotenuzasi", c);
+// console.log("perimetri", P);
+
+
+
+
+
+// Begin 13
+// Umumiy markazga bo'lgan ikkita aylana berilgan. R1,R2,(R1>R2) Ularning yuzasi S1 va S2, ularning ayirmasi S3 aniqlansin. S1 = PI * R1, S2 = PI * R2, S3 = PI * (R1 - R2)
+
+// let R1 = 5;
+// let R2 = 4;
+// const PI = 3.14;
+// const S1 = PI * R1;
+// const S2 = PI * R2;
+// const S3 = PI * (R1 - R2);
+
+// console.log("yuzasi",S1);
+// console.log("yuzasi",S2);
+// console.log("ayirmasi",S3);
+
+
+
+
+
+// Begin 14
+// Aylananing uzunligi L berilgan. Uning radiusi R va yuzasi S aniqlansin. L = 2 * PI * R  S = PI * R2
+
+// let L = 5;
+// const PI =3.14
+// const R = 2 / (2 * PI)
+// const S = PI * R ** 2
+
+
+// console.log("Radiusi:", R);
+// console.log("Yuzasi:", S);
+
+
+
+
+
+// begin 15
+// Aylananing yuzasi S berilgan. Uning diametri d va radiusi R aniqlansin. L=2*PI*R S=PI*R2
+
+// let S = 5;
+// const PI = 3.14;
+// const R = (S / PI) ** 2;
+// const L = 2 * PI * R;
+// const d = R * 2;
+
+// console.log("Diametri:", d);
+// console.log("Radiusi:", R); 
+
+
+
+
+// begin 16
+// sonlar o'qida ikkita nuqtaorasidagi masofa aniqlansin. |x2-x1|
+
+// let x1 = 10;
+// let x2 = 4;
+// const z = x1 - x2;
+
+// console.log(z);
